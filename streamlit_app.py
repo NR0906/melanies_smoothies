46dc3d45076f2dc914dab40f0c27daa04b532b00
NR0906 <A8876@Axtria.com>
2025-11-15 20:49:15 +0530

    Refactor order submission logic in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,14 +32,14 @@ if ingredients_list:
         sf_df= st.dataframe(data=smoothiefroot_response.json(), use_container_width= True)
         st.write(ingredients_string)
   
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
+        my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
             values ('"""+ingredients_string+""","""+name_on_order+"""')"""
-    st.write(my_insert_stmt)
-    time_to_insert= st.button('Submit Order')
+        st.write(my_insert_stmt)
+        time_to_insert= st.button('Submit Order')
 
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
+        if time_to_insert:
+          session.sql(my_insert_stmt).collect()
+          st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
 
 
